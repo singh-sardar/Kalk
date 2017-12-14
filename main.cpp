e1bf6d7c87856b1c7cb6605a9615fad2e57a5b5e
singh-sardar <singh-sardar@github.com>
2017-12-15 00:12:28 +0100

    Implementazione alcune funzioni di Vettore Generico e inserimento lable
    Inserimento di una label per il debug. vuole sempre un char* da mostrare.
    implementazione:
    -Costruttori
    -Distruttore
    -aggiungiElemento
    -PopBackElemento
    -cerca
    -rimuoviElemento

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
 #include "kalk.h"
 #include <QApplication>
 #include "vettoregenerico.h"
+#include <QLabel>
+
+
 
 int main(int argc, char *argv[])
 {
-    VettoreGenerico<int> v();
     QApplication a(argc, argv);
     kalk w;
+    QLabel* l= new QLabel(&w);
+
+    VettoreGenerico<char*> v;
+    char* s ;
+    v.aggiungiElemento("ciao");
+    v.aggiungiElemento("hei");
+    v.aggiungiElemento("wei");
+
+     s=v.popBackElemento();
+     l->setText(s);
+
+    /*if(v.rimuoviElemento("wei")){
+
+
+         l->setText("Si");
+    }
+    else
+        l->setText("No");*/
+
+
+    l->setFont(QFont("Times",20));
+    l->adjustSize();
     w.show();
 
     return a.exec();
--- vettoregenerico.h
+++ vettoregenerico.h
@@ -2,8 +2,12 @@
 #define VETTOREGENERICO_H
 #include <vector>
 #include <string>
+#include <algorithm>
+
+
 using std::vector;
 using std::string;
+using std::find;
 /*
  * classe base rappresentante un vettore generico di tipo T
  * */
@@ -17,13 +21,15 @@ private:
 public:
     VettoreGenerico();
     VettoreGenerico(vector<T>);
-    //NON CARICARE NEL MASTER IL FILE USER. CARICA SOLO IL RESTO
 
-    //QUANDO SI IMPLEMENTANO I METODI STARE ATTENTI A DECIDERE SE PASSARE PER REF CONST
-    //SVILUPPANDO COMMENTARE ADEGUAMENTE IL CODICE
-    void aggiungiElemento(T);//aggiunge l' elemento T
-    T popElemento(); // da adattare il tipo di ritorno in base al remove del vector
-    void rimuoviElemento(const T&); // rimuove l' elemento T
+
+
+    virtual void aggiungiElemento(const T&);//aggiunge l' elemento T in coda
+    T popBackElemento(); //rimuove un elemento in coda dal vettore e lo ritorna per valore
+    bool cerca(const T&);//ritorna true se un elemento è presente nel vettore
+    bool rimuoviElemento(const T&); // rimuove l' elemento T
+
+/*
     virtual string toString()const;
     int getSize()const;
     virtual VettoreGenerico operator+(const VettoreGenerico&);
@@ -33,16 +39,61 @@ public:
     VettoreGenerico& operator[](int);
     virtual bool operator ==(const VettoreGenerico&)const;
     virtual bool operator !=(const VettoreGenerico&)const;
-
+*/
 
     virtual ~VettoreGenerico();
 };
 
-template <class T>//costruttore senza parametri
+
+template <class T>
+VettoreGenerico<T>::~VettoreGenerico(){}
+
+//#H
+//costruttore senza parametri
+template <class T>
 VettoreGenerico<T>::VettoreGenerico():size(vettore.size()){}
 
+//#H
+//costruttore con un parametro di tipo vector<T>
 template <class T>//costruttore a un parametro di tipo vector<T>
 VettoreGenerico<T>::VettoreGenerico(vector<T> v):vettore(v),size(v.size()){}
+
+//#H
+//Aggiunge un elemento di tipo T in coda
+template <class T>
+void VettoreGenerico<T>::aggiungiElemento(const T& element){
+    vettore.push_back(element);
+}
+
+//#H
+//Elimina e ritorna l' elemento in coda
+template <class T>
+T VettoreGenerico<T>::popBackElemento(){
+    T aux = vettore.back();
+    vettore.pop_back();
+    return aux;
+
+}
+
+//#H
+//ritorna true se l' elemento di tipo T è presente altrimenti false
+template <class T>
+bool VettoreGenerico<T>::cerca(const T& element) {
+    return (find(vettore.begin(), vettore.end(), element)!= vettore.end());
+}
+
+//#H
+//rimuove l'elemento di tipo T dal vettore e ritorna true solo se l'elemento era presente nel vettore ed è stato rimosso
+template <class T>
+bool VettoreGenerico<T>::rimuoviElemento(const T& element){
+    typename vector<T>::iterator it = find(vettore.begin(), vettore.end(), element);
+    if( it != vettore.end()) {
+        vettore.erase(it);
+        return true;
+    }else {
+        return false;
+    }
+}
 
 
 
