babe4318333dbff06c46dc567cd8784df71bace8
singh-sardar <singh-sardar@github.com>
2017-12-18 23:46:10 +0100

    Definizione metodi classe Colore
    Definizione dei metodo principali della classe colore

--- colore.cpp
+++ colore.cpp
@@ -1,6 +1,10 @@
 #include "colore.h"
 
-Colore::Colore()
+Colore::Colore():r(0),g(0),b(0),a(1.0)
 {
+
+}
+
+Colore::Colore(int red, int green, int blue, float alpha):r(red),g(green),b(blue),a(alpha){
 
 }
--- colore.h
+++ colore.h
@@ -4,8 +4,44 @@
 
 class Colore
 {
+private:
+    int r, g, b; //rappresentano un colore in formato red, green,blue
+    float a; //alpha compreso tra 0.0 e 1.0
+
 public:
-    Colore();
+    Colore();// rappresenta il colore nero
+    Colore(int, int, int, float =1.0);
+
+    void modificaColore(int, int, int , float=1.0);
+    void setR(int);
+    void setG(int);
+    void setB(int);
+    void setA(float);
+    void getR(int)const;
+    void getG(int)const;
+    void getB(int)const;
+    void getA(float)const;
+
+
+
+    bool operator==(const Colore &)const;
+    bool operator !=(const Colore &)const;
+    Colore operator +(const Colore&);// somma tra 2 colori
+    Colore operator-(const Colore &);// differenza tra 2 colori
+    Colore operator*(const Colore&);//modulazione di un colore
+    Colore operator/(const Colore&);//modulazione di un colore
+    Colore& operator *(int);//scala le componenti del colore basandosi sul intero
+    Colore complementare()const;
+    Colore splitComplementare()const;
+    Colore complementare()const;
+    bool Caldo();//ritorna true se il colore è un colore caldo
+    int ScuroChiaro();//ritorna un intero compreso tra 0 e 255 che indica se un colore è chiaro o scuro. cosi si puo decidere se mettere il testo in bianco o nero avendo di sfondo il colore this
+
+     //colore analogo To search
+
+    //Decidere se mettere le conversioni da rgba a hex/hsl come metodi o come classi derivate
+
+
 };
 
 #endif // COLORE_H
